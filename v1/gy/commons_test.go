204e598e4cedc5f7bc073a5d44922e5e71afe994
Vincenzo Pupillo <v.pupillo@gmail.com>
2016-04-24 13:27:12 +0200

    Add some examples and benchmarks

---
+++ v1/gy/commons_test.go
@@ -0,0 +1,33 @@
+package gy
+
+import (
+	"log"
+)
+
+const (
+	vLength = 1024 * 1024 // test vector lenght
+)
+
+func checkStatus(status YepStatus) {
+	log.Printf("Status: %s\n", status.Name())
+}
+
+func gyInit() {
+	status, error := Init()
+
+	if error != nil {
+		log.Fatalf("Error %v\n", error)
+	}
+
+	log.Printf("Init status: %v\n", status.Name())
+
+}
+
+func gyRelease() {
+	status, error := Release()
+
+	if error != nil {
+		log.Fatalf("Error: %v\n", error)
+	}
+	log.Printf("Release status: %v\n", status.Name())
+}
