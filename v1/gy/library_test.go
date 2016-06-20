9bcd906713dd80bd4fc018cc6b41543d9b913dc5
Vincenzo Pupillo <v.pupillo@gmail.com>
2016-06-20 22:04:52 +0200

    Add some tests and benchmarks

---
+++ v1/gy/library_test.go
@@ -0,0 +1,35 @@
+package gy
+
+import (
+	"log"
+	"testing"
+)
+
+func TestLibrary(t *testing.T) {
+
+	status, error := Init()
+
+	if error != nil {
+		log.Fatalf("Error %v\n", error)
+	}
+
+	log.Printf("Init status: %v\n", status.Name())
+
+	var cm YepCpuMicroarchitecture
+	status, error = cm.CPUMicroarchitecture()
+
+	if error != nil {
+		log.Fatalf("Error: %v\n", error)
+	}
+
+	log.Printf("Microarch: %v, status: %v\n", cm, status.Name())
+
+	status, error = Release()
+
+	if error != nil {
+		log.Fatalf("Error: %v\n", error)
+	}
+
+	log.Printf("Release status: %v\n", status.Name())
+
+}
